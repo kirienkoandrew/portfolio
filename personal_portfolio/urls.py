fa6b152c961404904ecd269592e9a977cc7da698
Andrew Kirienko <kir.andrew@gmail.com>
2022-01-28 17:50:44 +0700

    добавил отображение моделей из бд

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from portfolio import views
+from portfolio import views as port
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    
+    path('', port.home),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
