f26fddeb469506d6c4635f12b80e2104e81d13cd
kirienkoandrew <kir.andrew@gmail.com>
2022-01-31 06:30:45 +0000

    удалил старый хлам

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+y34cga(e=b0bk$14_yp9lwg52vmhtdq-p#xbp5srdnl$+5l$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kirienkoandrew.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
